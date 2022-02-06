06509a6b87f53d9d2a8451d4debea44351cb1d0b
Redbeard29 <ben@benshermanmusic.com>
2022-02-06 14:19:21 -0500

    solved double or nothing

--- Projects/Ex_Files_Java_Code/Exercise Files/01_03/begin/DoubleOrNothing/src/main/java/DoubleOrNothing.java
+++ Projects/Ex_Files_Java_Code/Exercise Files/01_03/begin/DoubleOrNothing/src/main/java/DoubleOrNothing.java
@@ -1,6 +1,53 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class DoubleOrNothing {
 
+    public static Scanner scanner = new Scanner(System.in);
+    public static Random rand = new Random();
+
+    public boolean wantsToContinue(){
+
+        char response = scanner.next().charAt(0);
+        while(!(Character.toUpperCase(response) == 'Y' || Character.toUpperCase(response) == 'N')){
+            System.out.println("That input is invalid. Please enter Y or N.");
+            response = scanner.next().charAt(0);
+        }
+
+        if(Character.toUpperCase(response) == 'Y'){
+            return true;
+        }
+
+        return false;
+    }
+
     public void playGame() {
+
+        int points = 10;
+        boolean userHasPoints = true;
+        boolean wantsToContinue;
+
+        do{
+            System.out.println("You have " + points + " points. Would you like to take a chance to double your points or walk away? Y/N");
+            wantsToContinue = wantsToContinue();
+            if(wantsToContinue){
+                int toDouble = rand.nextInt(4);
+                if(toDouble == 0){
+                    System.out.print("You lose! ");
+                    points = 0;
+                    userHasPoints = false;
+                }
+                else{
+                    System.out.print("You have doubled your points! ");
+                    points = points * 2;
+                }
+            }
+
+        }while(userHasPoints && wantsToContinue);
+
+        scanner.close();
+
+        System.out.println("You have ended the game with " + points + " points.");
 
     }
 }
